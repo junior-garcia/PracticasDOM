d303ae9fbb391d27168b8e030a6db635428d5c75
junior <jungar2806@gmail.com>
2020-12-10 20:59:04 -0500

    final pract cuenta regresiva

--- PracticasDom/script.js
+++ PracticasDom/script.js
@@ -1,12 +1,16 @@
+
 import hamburgerMenu from "./menu_hamburguesa.js";
 import {digitalClock,alarm} from "./reloj.js";
 import {moveBall,shortcuts} from "./teclado.js";
+import countdown from "./cuenta_regresiva.js";
+
 
 
 document.addEventListener("DOMContentLoaded",(e)=>{
     hamburgerMenu(".panel-btn",".panel",".menu a");
     digitalClock("#reloj","#activar-reloj","#desactivar-reloj");
-    alarm("../assets/alarma.mp3","#activar-alarma","#desactivar-alarma")
+    alarm("../assets/alarma.mp3","#activar-alarma","#desactivar-alarma");
+    countdown("countdown","feb 24,2021 05:23:12","feliz cumpleaños");
 });
 
 document.addEventListener("keydown", (e)=>{
